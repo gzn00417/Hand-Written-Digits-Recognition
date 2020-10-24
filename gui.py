e0be7b55e0c447aa8e4c0016d1a1f93b111bb16a
Guo Zhuoning <gzn00417@foxmail.com>
2020-10-24 10:33:47 +0800

    v3.0 Hand-Written Board For Real-Time Recognition

--- gui.py
+++ gui.py
@@ -1,7 +1,16 @@
-import wx
-from window import Window
+# import wx
+# from window import Window
 
-app = wx.App()
-SiteFrame = Window()
-SiteFrame.Show()
-app.MainLoop()
+# app = wx.App()
+# SiteFrame = Window()
+# SiteFrame.Show()
+# app.MainLoop()
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from written_board import WrittenBoard
+
+app = QApplication(sys.argv)
+board = WrittenBoard()  # 新建一个主界面
+board.show()  # 显示主界面
+exit(app.exec_())  # 进入消息循环
